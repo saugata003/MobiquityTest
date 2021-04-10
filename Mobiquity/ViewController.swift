6db6e097a131e3fa95965d0b13f80951f48b3baf
Saugata Chakraborty <saugata.chakroborty@azurecomputer.com>
2021-04-10 16:23:10 +0530

    Initial Commit

---
+++ Mobiquity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mobiquity
+//
+//  Created by Saugata Chakraborty on 10/04/21.
+//  Copyright © 2021 Azure. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
